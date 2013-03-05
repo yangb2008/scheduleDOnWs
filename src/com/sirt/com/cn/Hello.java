ae518efff2efc573ec2e67b8b610effccd4267e6
bobo <bobo@ThinkPadw530-PC>
2013-03-05 15:57:12 +0800

    System.out.println("improve implements3");

--- src/com/sirt/com/cn/Hello.java
+++ src/com/sirt/com/cn/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.print("print message");
 		System.out.println("improve implements");
+		System.out.println("improve implements333");
 	}
 }

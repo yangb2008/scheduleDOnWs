0b20d61218c40a9a4e56bfe858db53b537f44c69
bobo <bobo@ThinkPadw530-PC>
2013-03-05 15:58:22 +0800

    44

--- src/com/sirt/com/cn/Hello.java
+++ src/com/sirt/com/cn/Hello.java
@@ -3,7 +3,7 @@ package com.sirt.com.cn;
 public class Hello {
 	public static void main(String args[]) {
 		System.out.print("print message");
-		System.out.println("improve implements");
+		System.out.println("improve implements444");
 		System.out.println("improve implements333");
 	}
 }
